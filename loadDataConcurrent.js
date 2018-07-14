b013a860f0c5c36804a619057027f3ed5257a855
jansony1 <ncatyu@126.com>
2018-07-14 17:46:40 +0800

    Update loadDataConcurrent.js

--- loadDataConcurrent.js
+++ loadDataConcurrent.js
@@ -1,6 +1,15 @@
 var AWS = require("aws-sdk");
 var fs = require('fs');
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const log4js = require('log4js');
+
+
+log4js.configure({
+  appenders: { cheese: { type: 'file', filename: 'result.log' } },
+  categories: { default: { appenders: ['cheese'], level: 'info' } }
+});
+
+const logger = log4js.getLogger('cheese');
 
 
 AWS.config.update({
@@ -9,7 +18,9 @@ AWS.config.update({
 
 
 
-var docClient = new AWS.DynamoDB.DocumentClient();
+var docClient  = new AWS.DynamoDB.DocumentClient();
+
+//docClient.setMaxListeners(0);
 
 console.log("Importing movies into DynamoDB. Please wait.");
 
@@ -35,11 +46,13 @@ var questions = [
 inquirer
   .prompt(questions)
   .then(answers => {
-      var concurrence = answers.concurrence   
+      var concurrency = answers.concurrency   
 
       var timer = answers.timer  
+      console.log("answers",answers)
+
       //设置每秒并发
-      var myInterval=setInterval(sendDataConcurrent,1000, concurrence);
+      var myInterval=setInterval(sendDataConcurrent,1000, concurrency);
       //设置执行时间，执行时间过后会stop掉上面的并发
       setTimeout(()=>{
         clearTimeout(myInterval);
@@ -52,6 +65,8 @@ inquirer
 // 模拟一次api网络请求花费1s
 async function _a_ddb_put(result) {
   return new Promise((resolve, reject) => {
+
+
   	var params = {
         TableName: "Movies",
         Item: {
@@ -63,12 +78,14 @@ async function _a_ddb_put(result) {
 
     docClient.put(params, function(err, data) {
        if (err) {
+           logger.error("Unable to add movie", result, ". Error JSON:", JSON.stringify(err, null, 2))
            console.error("Unable to add movie", result, ". Error JSON:", JSON.stringify(err, null, 2));
-           reject();
+           reject(err);
        } else {
-       	   counter++
+       	   
+           logger.info("PutItem succeeded:", result, " #######time is :", new Date().getMinutes(), ":",new Date().getSeconds())
            console.log("PutItem succeeded:", result, " #######time is :", new Date().getMinutes(), ":",new Date().getSeconds());
-		   resolve();
+		       resolve();
        }
     });
    	
@@ -76,6 +93,8 @@ async function _a_ddb_put(result) {
 }
 
 async function sendDataConcurrent(concurrency) {
+
+
   const t1 = Date.now();
   const promises = [];
   for (let i = 1; i <= concurrency; i += 1) {
@@ -83,9 +102,8 @@ async function sendDataConcurrent(concurrency) {
   }
   const result = await Promise.all(promises)
   const t2 = Date.now();
-  console.log(`total cost: ${t2 - t1}ms.`);
+  console.log(`total cost: ${t2 - t1}ms.`)
+  logger.info(`total cost: ${t2 - t1}ms.`)
   return result;
 }
-
-
 
